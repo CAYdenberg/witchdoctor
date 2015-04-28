87ddefbcdda1167ef9017b92b1d20fa367b825c3
CAYdenberg <casey@graphos.ca>
2015-04-28 11:05:17 -0600

    Added wp_title back to head

--- templates/head.php
+++ templates/head.php
@@ -3,6 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title><?php wp_title( ' | ', true, 'right' ); ?></title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
   </head>
