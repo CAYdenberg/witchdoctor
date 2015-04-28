84b6a98bd80bf6fa9691178f82bb39aac6caac54
CAYdenberg <casey@graphos.ca>
2015-04-28 11:22:56 -0600

    Added namespaces to config.php

--- lib/config.php
+++ lib/config.php
@@ -19,7 +19,7 @@ add_theme_support('bootstrap-gallery');     // Enable Bootstrap's thumbnails com
 
 // Remove admin menus
 
-add_action( 'admin_menu', 'remove_admin_menus' );
+add_action( 'admin_menu', __NAMESPACE__.'\remove_admin_menus' );
 function remove_admin_menus() {
 	remove_menu_page('edit.php');
     remove_menu_page('edit-comments.php');
@@ -29,13 +29,23 @@ function remove_admin_menus() {
 
 // Remove Comments
 function df_disable_comments_post_types_support(){$post_types=get_post_types();foreach($post_types as $post_type){if(post_type_supports($post_type,'comments')){remove_post_type_support($post_type,'comments');remove_post_type_support($post_type,'trackbacks');}}}
-add_action('admin_init','df_disable_comments_post_types_support');function df_disable_comments_status(){return false;}
-add_filter('comments_open','df_disable_comments_status',20,2);add_filter('pings_open','df_disable_comments_status',20,2);function df_disable_comments_hide_existing_comments($comments){$comments=array();return $comments;}
-add_filter('comments_array','df_disable_comments_hide_existing_comments',10,2);function df_disable_comments_admin_menu(){remove_menu_page('edit-comments.php');}
-add_action('admin_menu','df_disable_comments_admin_menu');function df_disable_comments_admin_menu_redirect(){global $pagenow;if($pagenow==='edit-comments.php'){wp_redirect(admin_url());exit;}}
-add_action('admin_init','df_disable_comments_admin_menu_redirect');function df_disable_comments_dashboard(){remove_meta_box('dashboard_recent_comments','dashboard','normal');}
-add_action('admin_init','df_disable_comments_dashboard');function df_disable_comments_admin_bar(){if(is_admin_bar_showing()){remove_action('admin_bar_menu','wp_admin_bar_comments_menu',60);}}
-//add_action('init','df_disable_comments_admin_bar');
+add_action('admin_init', __NAMESPACE__.'\df_disable_comments_post_types_support');
+
+function df_disable_comments_status(){return false;}
+add_filter('comments_open',__NAMESPACE__.'\df_disable_comments_status',20,2);add_filter('pings_open','df_disable_comments_status',20,2);function df_disable_comments_hide_existing_comments($comments){$comments=array();return $comments;}
+add_filter('comments_array','df_disable_comments_hide_existing_comments',10,2);
+
+function df_disable_comments_admin_menu(){remove_menu_page('edit-comments.php');}
+add_action('admin_menu',__NAMESPACE__.'\df_disable_comments_admin_menu');
+
+function df_disable_comments_admin_menu_redirect(){global $pagenow;if($pagenow==='edit-comments.php'){wp_redirect(admin_url());exit;}}
+add_action('admin_init',__NAMESPACE__.'\df_disable_comments_admin_menu_redirect');
+
+function df_disable_comments_dashboard(){remove_meta_box('dashboard_recent_comments','dashboard','normal');}
+add_action('admin_init',__NAMESPACE__.'\df_disable_comments_dashboard');
+
+function df_disable_comments_admin_bar(){if(is_admin_bar_showing()){remove_action('admin_bar_menu','wp_admin_bar_comments_menu',60);}}
+add_action('init',__NAMESPACE__.'\df_disable_comments_admin_bar');
 
 // Remove junk from head
 remove_action('wp_head', 'rsd_link');
