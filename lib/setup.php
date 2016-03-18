5e0baa656e947ee9bcadde24b2c43c5ed701a188
CAYdenberg <casey@graphos.ca>
2016-03-18 09:53:59 -0600

    Moved main script to footer

--- lib/setup.php
+++ lib/setup.php
@@ -46,9 +46,7 @@ function assets() {
 
   wp_enqueue_style('sage/css', Assets\asset_path('styles/main.css'), false, null);
 
-
-
-  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), null, true);
+  wp_enqueue_script('sage/js', Assets\asset_path('scripts/main.js'), null, null, true);
 }
 add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
 
